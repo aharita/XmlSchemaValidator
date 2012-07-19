798b56bf88b4f7f2de66994771744d1ca89aad16
Alfonso Harita <alharita@gmail.com>
2012-07-18 23:27:14 -0700

    xdocument methods

---
+++ XmlSchemaValidatorLib.Tests/XmlSchemaValidatorTest.cs
@@ -0,0 +1,78 @@
+﻿using XmlSchemaValidatorLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Xml.Linq;
+
+namespace XmlSchemaValidatorLib.Tests
+{
+    /// <summary>
+    ///This is a test class for XmlSchemaValidatorTest and is intended
+    ///to contain all XmlSchemaValidatorTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class XmlSchemaValidatorTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+        
+        /// <summary>
+        ///A test for Validate
+        ///</summary>
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            var xmlDoc = XDocument.Load("books.xml");
+            var xmlSchema = XDocument.Load("books.xsd");
+            const string xmlns = "urn:books";
+
+            XmlSchemaValidator.Instance.Validate(xmlDoc, xmlSchema, xmlns);
+
+            Assert.IsTrue(true, "Exception not thrown");
+        }
+    }
+}
---
+++ XmlSchemaValidatorLib/XmlSchemaValidator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace XmlSchemaValidatorLib
+{
+    public sealed class XmlSchemaValidator
+    {
+        #region Singleton
+
+        private static readonly Lazy<XmlSchemaValidator> Lazy =
+            new Lazy<XmlSchemaValidator>(() => new XmlSchemaValidator());
+
+        public static XmlSchemaValidator Instance
+        {
+            get { return Lazy.Value; }
+        }
+
+        private XmlSchemaValidator()
+        {
+        }
+
+        #endregion
+
+        public void Validate(XDocument doc, XDocument schema, string xmlns)
+        {
+            var schemas = new XmlSchemaSet();
+            schemas.Add(xmlns, XmlReader.Create(new StringReader(schema.ToString())));
+
+            doc.Validate(schemas, (sender, args) =>
+            {
+                throw new Exception(args.Message, args.Exception);
+            }, true);
+        }
+
+        public void Validate(XDocument doc, XDocument schema)
+        {
+            Validate(doc, schema, "");
+        }
+    }
+}
